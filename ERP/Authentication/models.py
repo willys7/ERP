75731e8f81ba2280b52f4588d397fea9f9755aa8
willys7 <wilfuentes0320@gmail.com>
2017-08-21 23:20:54 -0600

    add user model

--- ERP/Authentication/models.py
+++ ERP/Authentication/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    user_name = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+    email = models.EmailField(verbose_name='email address', max_length=254, unique=True)
+    def __str__(self):
+        return self.user_name
