af85748e239649d7274462f4d347fdf3fd3937c2
willys7 <wilfuentes0320@gmail.com>
2017-08-22 23:49:34 -0600

    add inventory model

--- ERP/Inventory/models.py
+++ ERP/Inventory/models.py
@@ -16,8 +16,19 @@ class Store(models.Model):
     phone = models.IntegerField()
     email = models.EmailField(verbose_name='email address', max_length=254, unique=True)
 
+
+#Ingredient Model
 class Ingredient(models.Model):
     ingredient_guid = models.CharField(max_length=250, primary_key=True, unique=True)
     name = models.CharField(max_length=100, unique=True)
     _type = models.CharField(max_length=100)
     expiration_date = models.DateField(blank=True, null=True)
+
+
+#Inventory Model
+class Inventory(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+    
