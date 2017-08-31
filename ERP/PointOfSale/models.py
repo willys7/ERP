176b09cb120130ec6596caf29cf25f559323c2f7
willys7 <wilfuentes0320@gmail.com>
2017-08-31 13:24:54 -0600

    add some Managers to models

--- ERP/PointOfSale/models.py
+++ ERP/PointOfSale/models.py
@@ -6,24 +6,58 @@ from django.db import models
 # Create your models here.
 
 #Buyer Model
+class BuyerManager(models.Manager):
+    def create_new_buyer(self, buyer):
+        try:
+            if buyer.address == "":
+                buyer.address = None
+            if buyer.phone == "":
+                buyer.address = None
+    
+            buyer_model = self.create(name=buyer.name, nit=buyer.nit, address=buyer.address,
+                phone=buyer.phone)
+            return buyer_model
+        except Exception, e:
+            raise Exception("Invalid transaction data: " + str(e))
+
 class Buyer(models.Model):
     name = models.CharField(max_length=100)
     nit = models.CharField(max_length=254, unique=True)
     address = models.CharField(max_length=254, blank=True, null=True)
-    phone = models.IntegerField()
-    #objects = PurchaseManager()
+    phone = models.IntegerField(blank=True, null=True)
+    objects = BuyerManager()
 
 #Invoice Model
+class InvoiceManager(models.Manager):
+    def create_new_invoice(self, invoice, buyer):
+        try:
+            invoice_model = self.create(buyer=buyer, amount=invoice.amount)
+            return invoice_model
+        except Exception, e:
+            raise Exception("Invalid transaction data: " + str(e))
+
 class Invoice(models.Model):
     buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
     amount = models.FloatField()
     date = models.DateField(auto_now=True)
-    #objects = PurchaseManager()
+    objects = InvoiceManager()
+
+
+#Product Model
+class ProductManager(models.Manager):
+    def create_new_invoice(self, product):
+        try:
+            product_model = self.create(product_guid=product.product_guid, name=product.name,
+                price=product.price)
+            return product_model
+        except Exception, e:
+            raise Exception("Invalid transaction data: " + str(e))
 
 class Product(models.Model):
     product_guid = models.CharField(max_length=250, primary_key=True, unique=True)
     name = models.CharField(max_length=250, unique= True)
     price = models.FloatField()
+    objects = ProductManager()
 
 class Recipe(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
