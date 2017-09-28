3219c0e53799831570954e2176ea8406eb0be3e7
willys7 <wilfuentes0320@gmail.com>
2017-09-27 20:15:27 -0600

    Update settings.py
    returned root password

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'root',
         'HOST': 'db',
         'PORT': '5432'
     }
