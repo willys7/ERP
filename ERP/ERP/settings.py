a5344032fe7fb802a083c0e9ebf3c57e1fe2c41f
juankboix1309 <juankboix@hotmail.com>
2017-10-02 12:29:38 -0600

    Update settings.py
    change Database name to postgres for testing

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'ERP.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ERP',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
