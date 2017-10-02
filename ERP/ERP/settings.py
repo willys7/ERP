77f66db9c13fb1cc6da6f0b0c061ea9e0701cf4f
juankboix1309 <juankboix@hotmail.com>
2017-10-02 11:48:42 -0600

    Update settings.py
    Erased PASSWORD from database config for testing

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ERP',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432'
     }
