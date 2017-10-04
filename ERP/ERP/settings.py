eda773398a5621f981ded8685497d27977bf4ba1
juankboix1309 <juankboix@hotmail.com>
2017-10-03 21:05:42 -0600

    Update settings.py
    remove password for build on aws ec2 server

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -83,7 +83,6 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432'
     }
