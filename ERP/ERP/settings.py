10ef0a7d84ffc24e74170bb7dff219017000acef
juankboix1309 <juankboix@hotmail.com>
2017-10-03 21:12:44 -0600

    Update settings.py
    added a missing comma

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'postgres'
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432'
     }
