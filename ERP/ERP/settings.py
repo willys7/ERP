e90a5634191a2efeb4385293120439e36149e475
juankboix1309 <juankboix@hotmail.com>
2017-10-03 21:06:58 -0600

    Update settings.py
    returned password for aws ec2 testing

--- ERP/ERP/settings.py
+++ ERP/ERP/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
+        'PASSWORD': 'postgres'
         'HOST': 'localhost',
         'PORT': '5432'
     }
